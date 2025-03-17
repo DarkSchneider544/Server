0186e4fa91898d898c7f055027e91119a8649531
DarkSchneider544 <122155672+DarkSchneider544@users.noreply.github.com>
2025-03-17 19:58:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from waitress import serve
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask is running on a local server!"
+    return "Hello, This is a HTTP-based web service!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3099, debug=True)
+    serve(app, host="0.0.0.0", port=3099)
